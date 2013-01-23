5e069ea0f5b8f1d7e326ef343eb92126a98e274c
Shawn Grimes <shawn@campfireapps.com>
2013-01-23 02:04:34 -0500

    Initial Commit

---
+++ MoleWhack/main.lua
@@ -0,0 +1,37 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- Your code here
+
+--[[
+local greenBackground=display.newImageRect("images/bg_green.png",1024,768)
+greenBackground.x=display.contentCenterX
+greenBackground.y=display.contentCenterY
+]]--
+
+
+    local myMole=display.newImageRect("images/mole_1.png",178,200)
+    myMole.x=display.contentCenterX
+	myMole.y=display.contentCenterY
+
+    local grassUpper=display.newImageRect("images/grass_upper.png",1024,384)
+    grassUpper.x=display.contentCenterX
+    grassUpper:setReferencePoint(display.TopCenterReferencePoint)
+    grassUpper.y=0
+
+    local grassLower=display.newImageRect("images/grass_lower.png",1024,384)
+    grassLower.x=display.contentCenterX
+    grassLower:setReferencePoint(display.BottomCenterReferencePoint)
+    grassLower.y=display.contentHeight
+    
+    grassUpper:toBack()
+    myMole:toFront()
+    grassLower:toFront()
+    
+    local bgDirt=display.newImageRect("images/bg_dirt.png",1024,768)
+    bgDirt.x=display.contentCenterX
+    bgDirt.y=display.contentCenterY
+    bgDirt:toBack()
